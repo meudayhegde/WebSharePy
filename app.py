2c9fdfa4275002da5c3eadfa1f13dad7ebe4c34e
Udaya Bhaskara Hegde <udaya@coleague.com>
2019-09-11 18:23:48 +0530

    few fixes for upload files

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
-from flask import Flask,render_template, send_file, send_from_directory, redirect, url_for, request
+import logging
+from flask import Flask,render_template, send_file, send_from_directory, redirect, url_for, request, make_response
 from sys import argv
 from os import environ,walk,listdir, mkdir
-from os.path import relpath,isdir,join, exists, getsize, getmtime
+from os.path import relpath,isdir,join, exists, getsize, getmtime, sep
 from sys import argv
 from time import ctime
+from werkzeug.utils import secure_filename
 import json
 
 class FileUtils():
@@ -47,7 +49,7 @@ class PyExplorer():
 		self.app.config['CURRENT_PATH']=path
 		self.app.config["CURRENT_ROUTE"]='/'
 		self.debug=debug
-		
+		self.log = logging.getLogger('pydrop')
 		self.create_routes()
 		
 	
@@ -79,18 +81,18 @@ class PyExplorer():
 	
 		@self.app.route('/templates/<path:varargs>')
 		def styles(varargs):
-			return send_file("templates/"+varargs,as_attachment=False)
+			return send_file(("templates/"+varargs).replace('/',sep),as_attachment=False)
 		
 		@self.app.route('/download/<path:varargs>')
 		def download(varargs):
-			path=join(self.app.config['PATH'],varargs)
+			path=join(self.app.config['PATH'],varargs.replace('/',sep))
 			return send_from_directory(path,as_attachment=True)
 		
 		@self.app.route('/mkdir/<path:varargs>',methods=['POST'])
 		def mk_dir(varargs):
 			try:
 				form=request.form
-				mkdir(join(self.app.config['PATH'],varargs,form['dirname']))
+				mkdir(join(self.app.config['PATH'],varargs.replace('/',sep),form['dirname']))
 				return redirect('/explore/'+varargs+'/'+form['dirname'])
 			except Exception as ex:
 				return '<h1>Failed to create folder '+ str(ex) +'</h1>'
@@ -103,53 +105,64 @@ class PyExplorer():
 				return redirect('/explore/'+form['dirname'])
 			except Exception as ex:
 				return '<h1>Failed to create folder '+ str(ex) +'</h1>'
+
+		@self.app.route("/upload/<path:varargs>", methods=["POST"])
+		def upload_(varargs):
+			return self.upload(join(self.app.config['PATH'],varargs.replace('/',sep)),varargs)
 			
-		
-		@self.app.route("/upload", methods=["POST"])
+		@self.app.route("/upload/", methods=["POST"])
 		def upload():
-			"""Handle the upload of a file."""
-			form = request.form
+			return self.upload(self.app.config['PATH'],'.')
+			
+	def upload(self,path,route):
+		file = request.files['file']
+		save_path = join(path, secure_filename(file.filename))
+		current_chunk = int(request.form['dzchunkindex'])
 
-			# Is the upload using Ajax, or a direct POST by the form?
-			is_ajax = False
-			if form.get("__ajax", None) == "true":
-				is_ajax = True
+		# If the file already exists it's ok if we are appending to it,
+		# but not if it's new file that would overwrite the existing one
+		if exists(save_path) and current_chunk == 0:
+			# 400 and 500s will tell dropzone that an error occurred and show an error
+			return make_response(('File already exists', 400))
 
-			# Target folder for these uploads.
-			target = self.app.config["CURRENT_PATH"]
-			try:
-				if not isdir(target) and not exists(target): os.mkdir(target)
-			except:
-				if is_ajax:
-					return self.ajax_response(False, "Couldn't create upload directory: {}".format(target))
-				else:
-					return "Couldn't create upload directory: {}".format(target)
+		try:
+			with open(save_path, 'ab') as f:
+				f.seek(int(request.form['dzchunkbyteoffset']))
+				f.write(file.stream.read())
+		except OSError:
+			# log.exception will include the traceback so we can see what's wrong 
+			self.log.exception('Could not write to file')
+			return make_response(("Not sure why,"
+								  " but we couldn't write the file to disk", 500))
 
-			print("=== Form Data ===")
-			for key, value in list(form.items()):
-				print(key, "=>", value)
+		total_chunks = int(request.form['dztotalchunkcount'])
 
-			for upload in request.files.getlist("file"):
-				filename = upload.filename.rsplit("/")[0]
-				destination = join(target, filename)
-				print("Accept incoming file:", filename)
-				print("Save it to:", destination)
-				upload.save(destination)
-
-			if is_ajax:
-				return self.ajax_response(True, "/explore"+self.app.config["CURRENT_ROUTE"])
+		if current_chunk + 1 == total_chunks:
+			# This was the last chunk, the file should be complete and the size we expect
+			if getsize(save_path) != int(request.form['dztotalfilesize']):
+				self.log.error(f"File {file.filename} was completed, "
+						  f"but has a size mismatch."
+						  f"Was {getsize(save_path)} but we"
+						  f" expected {request.form['dztotalfilesize']} ")
+				return make_response(('Size mismatch', 500))
 			else:
-				return redirect("/explore"+self.app.config["CURRENT_ROUTE"])
+				self.log.info(f'File {file.filename} has been uploaded successfully')
+		else:
+			self.log.debug(f'Chunk {current_chunk + 1} of {total_chunks} '
+						  f'for file {file.filename} complete')
+
+		return make_response(("Chunk upload successful", 200))			
 	
+    
 	def routes(self,varargs='.'):
-		path=join(self.app.config['PATH'],varargs)
+		path=join(self.app.config['PATH'],varargs.replace('/',sep))
 		if not isdir(path) : return send_file(path,as_attachment=True)
 		
 		dirs=[FileUtils(join(path,dir)) for dir in listdir(path) if isdir(join(path,dir))]
 		dirs.sort(key=lambda x: x.name)
 		files=[FileUtils(join(path,file)) for file in listdir(path) if file not in [dir.name for dir in dirs]]
 		files.sort(key=lambda x: x.name)
-		disp_path=[self.app.config['PATH'].split('/')[-1]]
+		disp_path=[self.app.config['PATH'].split(sep)[-1]]
 		disp_dict={disp_path[0]:'/'}
 		if varargs != '.':
 			var=varargs.split('/')
